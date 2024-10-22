f773e5618f1c0f0f276f19fcc3e76e7b65710533
katel-web <katelemass@gmail.com>
2024-10-22 16:42:02 +0100

    imported axios, added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0].definition);
+  }
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiKey = `8ff30efd06o4d12f7ftb8b44c4ad300b`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    // documentation: https://www.shecodes.io/learn/apis/dictionary
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function runKeywordChange(event) {
