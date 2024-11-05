0e54c4fccd190f4fa1d0816d9651b3c27a4b7821
katel-web <katelemass@gmail.com>
2024-11-05 11:24:43 +0000

    Added link to personal website portfolio

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ export default function App() {
     <div className="App container">
       <header className="App-header">Dictionary App </header>
       <Dictionary defaultKeyword="sunset" />
-      <footer>
-        Coded by Kate Lemass and{" "}
+      <footer className="mb-4">
+        Coded by{" "}
+        <a href="https://katelemass.com/" target="_blank" rel="noreferrer">
+          Kate Lemass
+        </a>{" "}
+        and{" "}
         <a
           href="https://github.com/katel-web/dictionary-project"
           target="_blank"
